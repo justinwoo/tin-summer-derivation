34c2d7539edf46096bb4b52639361674b143fdc3
justinwoo <moomoowoo@gmail.com>
2020-01-09 11:10:55 +0200

    add completion

--- default.nix
+++ default.nix
@@ -6,10 +6,20 @@ let
     sha256 = "1wywib06c9bq0m36nf9jxwknkv0zisgkcm78p7q25nfr09plcsz2";
   };
 
-in pkgs.runCommand "tin-summer" {
+  completion = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/justinwoo/sn-completion-bash/daf077e9a3a89e24301ff261720fed7f6d839e72/sn_completion.bash";
+    sha256 = "1dg1wlsw82srqr19gbrdza9z8l2jmpmn4kxi3r13xmfhf6bh59q6";
+  };
+
+in
+pkgs.runCommand "tin-summer" {
   name = "tin-summer";
   inherit src;
 } ''
   mkdir -p $out/bin
   tar xf $src -C $out/bin
+
+  COMPLETION=$out/etc/bash_completion.d/sn-completion.bash
+  mkdir -p $(dirname $COMPLETION)
+  ln -s ${completion} $COMPLETION
 ''
